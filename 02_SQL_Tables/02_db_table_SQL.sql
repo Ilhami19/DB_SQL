4f27c399113befe28611b1cb5d5cec432021a0fc
Ilhami19 <ilhamipolat72@gmail.com>
2021-07-06 08:12:19 +0000

    Insert into

--- 02_SQL_Tables/02_db_table_SQL.sql
+++ 02_SQL_Tables/02_db_table_SQL.sql
@@ -3,5 +3,38 @@
 /*         Datenbanken auf Server anzeigen        */
 SHOW DATABASES;
 
+/* DB boo löschen, falls vorhanden */
+DROP DATABASE IF EXISTS boo;
+
+
+/* DB boo anlegen, falls noch nicht vorhanden */
+CREATE DATABASE IF NOT EXISTS boo;
+
+/* DB auswählen */
+USE boo;
+
+CREATE TABLE IF NOT EXISTS test
+(
+    name VARCHAR(20),
+    age INT
+);
+
+SHOW TABLES;
+
+/* Struktur der TAbelle anzeigen  */
+DESCRIBE test;
+
+/* ---- DATEN ------ */
+INSERT INTO test(name, age) VALUES ("Grizabella", 29);
+#INSERT INTO test(name, age) VALUES ("Alonzo", 35);
+INSERT INTO test VALUES();
+
+SELECT * from test;
+
+#DROP TABLE IF EXISTS test;
+
+#SHOW TABLES;
+
+
 
 
