2050b5fe0e3ed048d40ef46c7e6200c20bf9a115
Ilhami19 <ilhamipolat72@gmail.com>
2021-07-07 08:42:59 +0000

    update 03/2:userDB mit UNIQUE + SHA1()

--- 03_userDB/03a_db_userDB_SQL.sql
+++ 03_userDB/03a_db_userDB_SQL.sql
@@ -9,15 +9,24 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users
 (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_name VARCHAR(20) NOT NULL,
+    user_name VARCHAR(20) UNIQUE NOT NULL,
+    user_pwd VARCHAR(40) NOT NULL,
     first_name VARCHAR(20) NOT NULL,
     family_name VARCHAR(20) NOT NULL
 );
 
 DESCRIBE users;
 
-INSERT INTO users(user_name, first_name, family_name) VALUES ("max", "Max", "Mütze");
-INSERT INTO users(user_name, first_name, family_name) VALUES ("maxine", "Maxine", "Mütze");
-INSERT INTO users(user_name, first_name, family_name) VALUES ("max", "Max", "Mützerich");
+/* Daten - Nachteil: PWD offen lesbar !!
+INSERT INTO users(user_name, user_pwd, first_name, family_name) VALUES ("max", "1234", "Max", "Mütze");
+INSERT INTO users(user_name, user_pwd, first_name, family_name) VALUES ("maxine", "#7Dx0", "Maxine", "Mütze");
+INSERT INTO users(user_name, user_pwd, first_name, family_name) VALUES ("max2", "user1234", "Max", "Mützerich");
+*/
+
+/* Daten - Besser: PWD verschülüsselt !! */
+INSERT INTO users(user_name, user_pwd, first_name, family_name) VALUES ("max", sha1("1234"), "Max", "Mütze");
+INSERT INTO users(user_name, user_pwd, first_name, family_name) VALUES ("maxine", sha1("#7Dx0"), "Maxine", "Mütze");
+INSERT INTO users(user_name, user_pwd, first_name, family_name) VALUES ("max2", sha1("user1234"), "Max", "Mützerich");
+
 
 SELECT * FROM users;
